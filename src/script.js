a00d190ab9b04c0c0fbc9a67a014bc1989972ed2
Yo Yehudi <yochannah@gmail.com>
2017-05-24 15:19:42 +0100

    move styles to separate sheet where possible, add unit of information basics and state variable basics

--- src/script.js
+++ src/script.js
@@ -1,3 +1,11 @@
+var styles = {
+  textSize: "8", //I know, it's numbers in strings. Don't cry.
+  corners: "5"
+}, uoiTypes = {
+  protein : "mt:prot"
+};
+
+
 var interaction;
 document.addEventListener("DOMContentLoaded", function(event) {
     $.get({
@@ -6,26 +14,86 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }, function(data) {
 
         new MIModel(data).load().then(function(model) {
-          //  console.log("model", model);
+
+          function UnitOfInformation(info){
+            this.info = uoiTypes[info];
+            this.node = document.createElementNS("http://www.w3.org/2000/svg","g");
+            var text = document.createElementNS("http://www.w3.org/2000/svg","text")
+            text.appendChild(document.createTextNode(this.info));
+            var rect = document.createElementNS("http://www.w3.org/2000/svg","rect");
+
+            rect.setAttributeNS(null,"strokeWidth",1);
+            rect.setAttributeNS(null,"width",30);
+            rect.setAttributeNS(null,"height",10);
+            this.node.appendChild(rect);
+
+
+            text.setAttributeNS(null,"x","0");
+            text.setAttributeNS(null,"y","6");
+            text.setAttributeNS(null,"font-size",  styles.textSize);
+            this.node.appendChild(text);
+// TODO: Calc after render. Here it returns 0.
+//              this.textSize = text.getBBox();
+// rect.setAttributeNS(null,"width",this.textSize.width);
+// rect.setAttributeNS(null,"height",this.textSize.height);
+          }
+
+          function StateVariable(info){
+            this.info = info;
+            this.node = document.createElementNS("http://www.w3.org/2000/svg","g");
+            var text = document.createElementNS("http://www.w3.org/2000/svg","text")
+            text.appendChild(document.createTextNode(this.info));
+            var rect = document.createElementNS("http://www.w3.org/2000/svg","rect");
+
+            rect.setAttributeNS(null,"strokeWidth",1);
+            rect.setAttributeNS(null,"width",30);
+            rect.setAttributeNS(null,"rx",5);
+            rect.setAttributeNS(null,"ry",5);
+            rect.setAttributeNS(null,"height",10);
+            this.node.appendChild(rect);
+
+
+            text.setAttributeNS(null,"x","0");
+            text.setAttributeNS(null,"y","6");
+            text.setAttributeNS(null,"font-size",  styles.textSize);
+            this.node.appendChild(text);
+// TODO: Calc after render. Here it returns 0.
+//              this.textSize = text.getBBox();
+// rect.setAttributeNS(null,"width",this.textSize.width);
+// rect.setAttributeNS(null,"height",this.textSize.height);
+          }
 
 
             function Interactor(model) {
-              console.log("%cmodel","color:firebrick;font-weight:bold;",model);
+            //  console.log("%cmodel","color:firebrick;font-weight:bold;",model);
               this.init(model);
               return this;
             }
-            var location = 0
+
+            var location = 0; // temp var until we get positioning properly
+
+
             Interactor.prototype.init = function(model) {
               this.model = model;
-              this.node = document.createElementNS("http://www.w3.org/2000/svg","text");
-              this.node.setAttributeNS(null,"x",10);
-              location = location + 30;
-              this.node.setAttributeNS(null,"y",location);
-              console.log("%cmodel.attributes","color:turquoise;font-weight:bold;",model.attributes);
-              var label = document.createTextNode(model.attributes.label);
-              this.node.appendChild(label);
-            }
+              this.node = document.createElementNS("http://www.w3.org/2000/svg","g");
 
+              location = location + 30;
+              this.node.setAttributeNS(null,"x",10);
+              this.node.setAttributeNS(null,"y",location);
+
+
+              var text = document.createElementNS("http://www.w3.org/2000/svg","text");
+              var label = document.createTextNode(model.attributes.label);
+              text.appendChild(label);
+              text.setAttributeNS(null,"font-size", styles.textSize);
+
+            //  console.log("%cmodel.attributes","color:turquoise;font-weight:bold;",model.attributes);
+
+              this.node.appendChild(text);
+              this.node.setAttributeNS(null,"transform","translate(10," + location + ")");
+              this.node.uoi = new UnitOfInformation(this.model.attributes.type.name);
+              this.node.appendChild(this.node.uoi.node);
+            }
 
 
             var ComplexView = Backbone.View.extend({
@@ -40,6 +108,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
                     var mynode = document.createElementNS("http://www.w3.org/2000/svg","rect");
                     mynode.setAttributeNS(null,"x",10);
                     mynode.setAttributeNS(null,"y",10);
+                    mynode.setAttributeNS(null,"height",300);
                     this.el.appendChild(mynode);
 
                     try {
@@ -47,12 +116,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
                       var inter = new Interactor(interactor);
                       parent.el.appendChild(inter.node);
                     });
-                  } catch (e) {console.log("%ce","color:turquoise;font-weight:bold;",e);}
+                  } catch (e) {console.log("%ce","color:navajowhite;font-weight:bold;",e);}
                     return this;
                 }
 
             });
-
+            console.log("%cmodel","color:turquoise;font-weight:bold;",model);
             model.attributes.interactions.models.map(function(x, y){
               new ComplexView({
                   model: model,
