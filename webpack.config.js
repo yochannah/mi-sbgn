e95d8548a6bc1ffe691149e0a85e26d765f9d3b9
Yo Yehudi <yochannah@gmail.com>
2017-11-30 14:18:02 +0000

    added uglify to shrink file size of bundles, but cola doesn't bundle well and ends up with undefined variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,30 @@
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: {
-    main : './js/index.js',
-    dependencies : './js/dependencies.js'
-},
+    main: './js/index.js',
+    dependencies: './js/dependencies.js'
+  },
+  plugins: [
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        // mangle: {
+        //   reserved: ["$",
+        //     "_",
+        //     "Backbone",
+        //     "MIModel",
+        //     "cola",
+        //     "d3",
+        //     "corner",
+        //     "jstoxml"
+        //   ]
+        // }
+        mangle:false
+      }
+    })
+
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'mi-sbgn-[name].bundle.js'
